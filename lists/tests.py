7c8c55420d2a4156eb80ca8b75c9275408fc998f
Esthefanie Lanza <esthefanie.lanza@hotmart.com>
2020-09-10 22:57:46 -0300

    Apresentar Múltiplos Itens na Listagem

--- lists/tests.py
+++ lists/tests.py
@@ -14,11 +14,14 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
 
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
